de3a695c904384b6016a0fe6cdc71a53dc635671
Ulrich Freiberg <Ulrich@mirsoftware.com>
2017-09-30 17:31:06 +0200

    ModelBase: In need of a modelbase - first shot

---
+++ DisplayTargets/WrapTrackWeb/WrapTrack.Stf.WrapTrackWeb/WrapTrackWebShellModelBase.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WrapTrackWebShellModelBase.cs" company="Nordea">
+//   Copyright governed by Artistic license as described here:
+//   //          http://www.perlfoundation.org/artistic_license_2_0
+// </copyright>
+// <summary>
+//   The im shell model base.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WrapTrack.Stf.WrapTrackWeb
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using Mir.Stf.Utilities;
+
+    using OpenQA.Selenium;
+
+    using WrapTrack.Stf.Adapters.WebAdapter;
+
+    /// <summary>
+    /// The im shell model base.
+    /// </summary>
+    public abstract class WrapTrackWebShellModelBase : StfModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WrapTrackWebShellModelBase"/> class.
+        /// </summary>
+        /// <param name="wrapTrackWebShell">
+        /// The wrap track web shell.
+        /// </param>
+        protected WrapTrackWebShellModelBase(WrapTrackWebShell wrapTrackWebShell)
+        {
+            WrapTrackWebShell = wrapTrackWebShell;
+            WebAdapter = wrapTrackWebShell.WebAdapter;
+        }
+
+        /// <summary>
+        /// Gets or sets the wrap track web shell.
+        /// </summary>
+        protected WrapTrackWebShell WrapTrackWebShell { get; set; }
+
+        /// <summary>
+        /// Gets or sets the web adapter.
+        /// </summary>
+        protected IWebAdapter WebAdapter { get; set; }
+    }
+}
