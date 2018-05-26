ab2fd08c0d555515597e46154b549951d838a51e
wraptrack <pernille@11time.dk>
2018-05-26 20:34:22 +0200

    Hmm seems like I have to stage/commits these test cases before I can move on. Don't know why they where not part of solution any longer...

---
+++ UnitTests/WrapTrackApiTests/Collection/TestCase006.cs
@@ -0,0 +1,64 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TestCase006.cs" company="Mir Software">
+//   Copyright governed by Artistic license as described here:
+//          http://www.perlfoundation.org/artistic_license_2_0
+// </copyright>
+// <summary>
+//   Defines the CollectionTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WrapTrackWebTests.Collection
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using WrapTrack.Stf.WrapTrackWeb.Interfaces;
+
+    /// <summary>
+    /// The collection tests.
+    /// </summary>
+    [TestClass]
+    public class TestCase006 : WrapTrackTestScriptBase
+    {
+        /// <summary>
+        /// The test initialize.
+        /// </summary>
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            WrapTrackShell = Get<IWrapTrackWebShell>();
+        }
+
+        /// <summary>
+        /// The test clean up.
+        /// </summary>
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            WrapTrackShell?.CloseDown();
+        }
+
+        /// <summary>
+        /// The t c 006.
+        /// </summary>
+        [TestMethod]
+        public void Tc006()
+        {
+            WrapTrackShell.Login();
+
+            var me = WrapTrackShell.Me();
+            var collection = me.GetCollection();
+
+            StfAssert.IsNotNull("Got a MeProfile", me);
+            StfAssert.IsNotNull("Got my collection", collection);
+
+            var numBefore = collection.NumOfWraps();
+
+            collection.AddWrap("Ali Dover", "Hygge", "blue");
+
+            var numAfter = collection.NumOfWraps();
+
+            StfAssert.AreEqual("One more wrap in collection", numBefore + 1, numAfter);
+        }
+    }
+}
---
+++ UnitTests/WrapTrackApiTests/Collection/TestCase007.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TestCase007.cs" company="Mir Software">
+//   Copyright governed by Artistic license as described here:
+//          http://www.perlfoundation.org/artistic_license_2_0
+// </copyright>
+// <summary>
+//   Defines the CollectionTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WrapTrackWebTests.Collection
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using WrapTrack.Stf.WrapTrackWeb.Interfaces;
+
+    /// <summary>
+    /// Test of letting a wrap pass on from one user to another. 
+    /// The owner  initiatives this action.
+    /// Default date.
+    /// </summary>
+    [TestClass]
+    public class TestCase007 : WrapTrackTestScriptBase
+    {
+        /// <summary>
+        /// The test initialize.
+        /// </summary>
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            WrapTrackShell = Get<IWrapTrackWebShell>();
+            WrapTrackShell.Login(); 
+        }
+
+        /// <summary>
+        /// The test clean up.
+        /// </summary>
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            WrapTrackShell?.CloseDown();
+        }
+
+        /// <summary>
+        /// The TC007.
+        /// </summary>
+        [TestMethod]
+        public void Tc007()
+        {
+            var collection = GetCurrentUserCollection();
+
+            // Find a random wrap
+            var wrapToGo = collection.GetRandomWrap();
+            var wtId = wrapToGo.WtId;
+
+            StfAssert.IsNotNull("Got a random wrap", wrapToGo);
+
+            var anotherUser = GetAnotherUser(WrapTrackShell);
+            var passOn = wrapToGo.PassOn(anotherUser);
+
+            StfAssert.IsTrue("PassedOn", passOn);
+            StfAssert.IsTrue("PassedOn Validated", ValidatePassOn(wtId, anotherUser));
+        }
+    }
+}
