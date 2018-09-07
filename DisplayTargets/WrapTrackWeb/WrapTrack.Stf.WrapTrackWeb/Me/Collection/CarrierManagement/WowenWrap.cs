aa0acf2028f0d1343ba3072bf2f613d3739c9389
Ulrich Freiberg <Ulrich@mirsoftware.com>
2018-09-07 19:01:55 +0200

    WowenWrap: Fixed a compilation error

--- DisplayTargets/WrapTrackWeb/WrapTrack.Stf.WrapTrackWeb/Me/Collection/CarrierManagement/WowenWrap.cs
+++ DisplayTargets/WrapTrackWeb/WrapTrack.Stf.WrapTrackWeb/Me/Collection/CarrierManagement/WowenWrap.cs
@@ -1,7 +1,7 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="WowenWrap.cs" company="Mir Software">
 //   Copyright governed by Artistic license as described here:
-//   //          http://www.perlfoundation.org/artistic_license_2_0
+//          http://www.perlfoundation.org/artistic_license_2_0
 // </copyright>
 // <summary>
 //   Defines the StretchyWrap type.
@@ -18,10 +18,10 @@ namespace WrapTrack.Stf.WrapTrackWeb.Me.Collection.CarrierManagement
         /// <summary>
         /// Gets or sets a value indicating whether home made.
         /// </summary>
-        public bool HomeMade { get; set;
+        public bool HomeMade { get; set; }
 
         /// <summary>
-        /// Gets or sets name of home made wrap.
+        /// Gets or sets a value indicating whether name.
         /// </summary>
         public bool Name { get; set; }
     }
