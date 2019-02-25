9a2b5f3221411a16f89977b0cd6120b62ae91c8e
Ulrich Freiberg <Ulrich@mirsoftware.com>
2019-02-25 21:59:42 +0100

    AddCaririer: More carriers implementedf

---
+++ DisplayTargets/WrapTrackWeb/WrapTrack.Stf.WrapTrackWeb/Me/Collection/CarrierManagement/Podeagi.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Podeagi.cs" company="Mir Software">
+//   Copyright governed by Artistic license as described here:
+//          http://www.perlfoundation.org/artistic_license_2_0
+// </copyright>
+// <summary>
+//   Defines the Podeagi type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WrapTrack.Stf.WrapTrackWeb.Me.Collection.CarrierManagement
+{
+    using OpenQA.Selenium;
+
+    using WrapTrack.Stf.WrapTrackWeb.Interfaces;
+    using WrapTrack.Stf.WrapTrackWeb.Interfaces.Me.Collection.CarrierManagement;
+
+    /// <summary>
+    /// The HalfBuckleMeiTai wrap.
+    /// </summary>
+    public class Podeagi : CarrierBaseClass, IPodeagi
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Podeagi"/> class.
+        /// </summary>
+        /// <param name="wrapTrackWebShell">
+        /// The wrap track web shell.
+        /// </param>
+        public Podeagi(IWrapTrackWebShell wrapTrackWebShell)
+            : base(wrapTrackWebShell)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                var retVal = WebAdapter.GetText(By.Id("inpNameHomemade"));
+
+                return retVal;
+            }
+
+            set
+            {
+                WebAdapter.TextboxSetTextById("inpNameHomemade", value);
+            }
+        }
+    }
+}
